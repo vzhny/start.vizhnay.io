ef0e2ec20f31e670a51e9207ee9bafe34c173fa1
Diego Vizhnay <diego@vzhny.com>
2019-03-27 15:18:59 -0400

    📝 Added useLayoutEffect -- calling getGreeting to change doc title | 03/27

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { getGreeting } from '@/utils/utils';
 import Container from '@/components/Container/Container';
 import { app } from './App.module.scss';
 
 const App = () => {
+  useLayoutEffect(() => {
+    const currentHour = new Date().getHours();
+
+    document.title = getGreeting(currentHour);
+  }, []);
+
   return (
     <div className={app}>
       <Container>
