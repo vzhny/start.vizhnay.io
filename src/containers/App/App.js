1af71bca59af4b1ce3e9b1aadd5110107171b0e0
Diego Vizhnay <diego@vzhny.com>
2019-03-27 13:06:56 -0400

    🎉 Initial commit! | 03/27

---
+++ src/containers/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { app } from './App.module.scss';
+
+const App = () => {
+  return (
+    <div className={app}>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
