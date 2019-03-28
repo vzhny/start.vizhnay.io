ae5027555aa4f5078748f44b2c304b3cad744241
Diego Vizhnay <diego@vzhny.com>
2019-03-28 12:33:36 -0400

    📝 Created a backdrop component | 03/28

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { backdrop, visible, hidden } from './Backdrop.module.scss';
+
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/aria-role */
+
+const Backdrop = ({ isVisible, toggleVisibility }) => {
+  return (
+    <div
+      className={clsx(backdrop, isVisible ? visible : hidden)}
+      onClick={() => toggleVisibility(false)}
+      onKeyPress={() => toggleVisibility(false)}
+      role="backdrop"
+    />
+  );
+};
+
+Backdrop.defaultProps = {
+  isVisible: false,
+};
+
+Backdrop.propTypes = {
+  isVisible: PropTypes.bool,
+  toggleVisibility: PropTypes.func.isRequired,
+};
+
+export default Backdrop;
