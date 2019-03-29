47511f54433ba4b973b51799b2458c33f0d6b760
Diego Vizhnay <diego@vzhny.com>
2019-03-28 20:18:06 -0400

    📝 Added a default base url to axios | 03/28

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axois';
 import App from '@/containers/App/App';
 import '@/styles/scss/base.scss';
+
+axios.defaults.baseURL = 'https://api-start-vizhnay-io.herokuapp.com/api';
 
 ReactDOM.render(<App />, document.getElementById('app'));
