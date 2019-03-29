f67de3a0200754ed249c6a658268a1acc1dcedc7
Diego Vizhnay <diego@vzhny.com>
2019-03-28 20:19:23 -0400

    🐛 Fixed axios import typo | 03/28

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axois';
+import axios from 'axios';
 import App from '@/containers/App/App';
 import '@/styles/scss/base.scss';
 
