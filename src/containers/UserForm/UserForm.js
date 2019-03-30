7ad5a00fb5fed157359aff38bb5f2931a099b7b2
Diego Vizhnay <diego@vzhny.com>
2019-03-29 23:04:56 -0400

    🔨 Implemented formik as the form logic handler | 03/29 📝 Added form validation via yup | 03/29 📝 Added card footer with link to switch between forms | 03/29

--- src/containers/UserForm/UserForm.js
+++ src/containers/UserForm/UserForm.js
@@ -1,44 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Card, { CardHeader, CardBody } from '@/components/Card/Card';
-import { form, label, input, buttonGroup, registerButton, loginButton } from './UserForm.module.scss';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import to from 'await-to-js';
+import store from 'store';
+import clsx from 'clsx';
+import Card, { CardHeader, CardBody, CardFooter } from '@/components/Card/Card';
+import {
+  form,
+  label,
+  input,
+  validInput,
+  inputError,
+  errorMessage,
+  buttonGroup,
+  cancelButton,
+  submitButton,
+  toggleFormButton,
+} from './UserForm.module.scss';
+
+const UserFormSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email!')
+    .required('An email address is required.'),
+  password: Yup.string().required('A password is required.'),
+});
 
 const UserForm = ({ toggleVisibility }) => {
-  const registerNewAccount = e => {
-    e.preventDefault();
-    toggleVisibility(false);
-  };
+  const [formType, setFormType] = useState('login');
+  const [serverError, setServerError] = useState('');
 
-  const loginExistingAccount = e => {
-    e.preventDefault();
-    toggleVisibility(false);
+  const toggleBetweenForms = () => {
+    if (formType === 'login') {
+      setFormType('register');
+    } else {
+      setFormType('login');
+    }
   };
 
   return (
     <Card>
-      <CardHeader>
-        <h3>Login/Register</h3>
+      <CardHeader style={{ paddingTop: '1.5rem' }}>
+        <h3>{formType === 'login' ? 'Log into an existing account!' : 'Register a new account!'}</h3>
       </CardHeader>
       <CardBody>
-        <form className={form}>
-          <label className={label} htmlFor="email">
-            Email:
-            <input className={input} name="email" placeholder="hello@gmail.com" type="text" />
-          </label>
-          <label className={label} htmlFor="password">
-            Password:
-            <input className={input} name="password" placeholder="••••••" type="password" />
-          </label>
-          <div className={buttonGroup}>
-            <button className={registerButton} onClick={e => registerNewAccount(e)} type="submit">
-              Register
-            </button>
-            <button className={loginButton} onClick={e => loginExistingAccount(e)} type="submit">
-              Login
-            </button>
-          </div>
-        </form>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          onSubmit={async (values, { setSubmitting, resetForm }) => {
+            const [error, response] = await to(axios.post(`/auth/${formType}`, values));
+
+            if (error) {
+              setServerError('There was an error logging in, please try again later.');
+              setSubmitting(false);
+            } else {
+              const { jwt } = response.data;
+              store.set('token', jwt);
+              setSubmitting(false);
+              toggleVisibility(false);
+              resetForm({ email: '', password: '' });
+            }
+          }}
+          render={({ values, errors, touched, handleSubmit, handleChange, handleBlur, isSubmitting }) => (
+            <form className={form} onSubmit={e => handleSubmit(e)}>
+              <label className={label} htmlFor="email">
+                Email:
+                <input
+                  className={clsx(input, errors.email && inputError, !errors.email && touched.email && validInput)}
+                  disabled={isSubmitting}
+                  name="email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="hello@gmail.com"
+                  type="text"
+                  value={values.email}
+                />
+                {errors.email && touched.email ? <p className={errorMessage}>{errors.email}</p> : null}
+              </label>
+              <label className={label} htmlFor="password">
+                Password:
+                <input
+                  className={clsx(
+                    input,
+                    errors.password && inputError,
+                    !errors.password && touched.password && validInput
+                  )}
+                  disabled={isSubmitting}
+                  name="password"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="••••••"
+                  type="password"
+                  value={values.password}
+                />
+                {errors.password && touched.password ? (
+                  <p className={errorMessage} style={{ marginBottom: '0' }}>
+                    {errors.password}
+                  </p>
+                ) : null}
+              </label>
+              {serverError.length > 0 ? <p className={errorMessage}>{serverError}</p> : null}
+              <div className={buttonGroup}>
+                <button className={cancelButton} onClick={() => toggleVisibility(false)} type="button">
+                  Cancel
+                </button>
+                <button
+                  className={submitButton}
+                  disabled={!touched.email || !touched.password || errors.email || errors.password}
+                  type="submit"
+                >
+                  {formType === 'login' ? 'Login' : 'Register'}
+                </button>
+              </div>
+            </form>
+          )}
+          validationSchema={UserFormSchema}
+        />
       </CardBody>
+      <CardFooter>
+        <button className={toggleFormButton} onClick={() => toggleBetweenForms()} type="button">
+          {formType === 'login' ? 'Register a new account!' : 'Log into an existing account!'}
+        </button>
+      </CardFooter>
     </Card>
   );
 };
