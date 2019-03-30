745f307245996ab65d07c27d9d2080755a2cceed
Diego Vizhnay <diego@vzhny.com>
2019-03-29 22:49:02 -0400

    🔨 Spread any passed props to respective components | 03/29

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,14 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { card, cardHeader, cardBody, cardFooter } from './Card.module.scss';
 
-export const CardHeader = ({ children }) => <div className={cardHeader}>{children}</div>;
+export const CardHeader = ({ children, ...props }) => (
+  <div {...props} className={cardHeader}>
+    {children}
+  </div>
+);
 
-export const CardBody = ({ children }) => <div className={cardBody}>{children}</div>;
+export const CardBody = ({ children, ...props }) => (
+  <div {...props} className={cardBody}>
+    {children}
+  </div>
+);
 
-export const CardFooter = ({ children }) => <div className={cardFooter}>{children}</div>;
+export const CardFooter = ({ children, ...props }) => (
+  <div {...props} className={cardFooter}>
+    {children}
+  </div>
+);
 
-const Card = ({ children }) => {
-  return <div className={card}>{children}</div>;
+const Card = ({ children, ...props }) => {
+  return (
+    <div {...props} className={card}>
+      {children}
+    </div>
+  );
 };
 
 CardHeader.propTypes = {
