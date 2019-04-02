e2c18a45802b27d3f228d21094b5e0fb020d0746
Diego Vizhnay <diego@vzhny.com>
2019-04-02 12:45:31 -0400

    🔥 Removed collection css class from card body component | 04/02

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card, { CardBody } from '@/components/Card/Card';
-import { collection, link } from './Collection.module.scss';
+import { link } from './Collection.module.scss';
 
 const Collection = ({ category, links }) => {
   return (
     <Card>
-      <CardBody className={collection}>
+      <CardBody>
         {links.map(({ title, url, linkId }) => (
           <a key={linkId} className={link} href={url}>
             {title}
