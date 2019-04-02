dfd495b7ed358e2b97e51a186e7025969ea7b76c
Diego Vizhnay <diego@vzhny.com>
2019-04-01 22:51:06 -0400

    📝 Added link retrieval logic via http request to API | 04/01 📝 Added conditional check that retrieves links from local storage if already saved | 04/01

--- src/containers/LinksContainer/LinksContainer.js
+++ src/containers/LinksContainer/LinksContainer.js
@@ -1,28 +1,62 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useState, useLayoutEffect, useContext } from 'react';
+import to from 'await-to-js';
+import axios from 'axios';
+import store from 'store';
+import { useArray } from 'react-hanger';
 import { AuthContext } from '@/context/AuthContext';
 import Card, { CardBody } from '@/components/Card/Card';
 import { linksContainer } from './LinksContainer.module.scss';
+import Collection from '@/components/Collection/Collection';
 
 const LinksContainer = () => {
-  const [auth] = useContext(AuthContext);
+  const linkData = useArray([]);
+  const [serverError, setServerError] = useState('');
+  const [auth, setAuth] = useContext(AuthContext);
+
+  const retrieveLocalLinks = () => {
+    linkData.setValue(store.get('links'));
+  };
 
   const retrieveLinks = async () => {
-    if (auth) {
-      // GET request logic goes here
+    const token = store.get('token');
+
+    if (token) {
+      const [error, response] = await to(
+        axios.get('/links', {
+          headers: { Authorization: token },
+        })
+      );
+
+      if (error) {
+        setServerError('There was an error retrieving your links, please try again later.');
+
+        return;
+      }
+
+      store.set('links', response.data.links);
+      linkData.setValue(response.data.links);
+      setAuth(true);
     }
   };
 
-  useEffect(() => {
-    retrieveLinks();
+  useLayoutEffect(() => {
+    if (store.get('links')) {
+      setAuth(true);
+      retrieveLocalLinks();
+    } else {
+      retrieveLinks();
+    }
   }, [auth]);
 
   return (
     <div className={linksContainer}>
-      <Card>
-        <CardBody>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, similique?</p>
-        </CardBody>
-      </Card>
+      {auth ? (
+        linkData.value.map(({ category, links }) => <Collection key={category} category={category} links={links} />)
+      ) : (
+        <Card>
+          <CardBody>{serverError ? <p>{serverError}</p> : <p>Please log in to start adding links!</p>}</CardBody>
+        </Card>
+      )}
     </div>
   );
 };
