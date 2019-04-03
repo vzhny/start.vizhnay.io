19d654c57afa1b0008cf9bda139567fac2d3ada9
Diego Vizhnay <diego@vzhny.com>
2019-04-03 09:42:32 -0400

    📝 Added refresh context | 04/03 📝 Added links context | 04/03

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,7 @@
 import React, { useLayoutEffect } from 'react';
 import { getGreeting } from '@/utils/utils';
+import { RefreshProvider } from '@/state/context/RefreshContext';
+import { LinksProvider } from '@/state/context/LinksContext';
 import { AuthProvider } from '@/state/context/AuthContext';
 import SideMenu from '@/containers/SideMenu/SideMenu';
 import LinksContainer from '@/containers/LinksContainer/LinksContainer';
@@ -8,16 +10,19 @@ import { app } from './App.module.scss';
 const App = () => {
   useLayoutEffect(() => {
     const currentHour = new Date().getHours();
-
     document.title = getGreeting(currentHour);
   }, []);
 
   return (
     <div className={app}>
-      <AuthProvider>
-        <LinksContainer />
-        <SideMenu />
-      </AuthProvider>
+      <RefreshProvider>
+        <LinksProvider>
+          <AuthProvider>
+            <LinksContainer />
+            <SideMenu />
+          </AuthProvider>
+        </LinksProvider>
+      </RefreshProvider>
     </div>
   );
 };
