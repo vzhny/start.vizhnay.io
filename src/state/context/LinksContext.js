2b15eb1b5c27a39e31be30af473bbd26e807e37a
Diego Vizhnay <diego@vzhny.com>
2019-04-04 19:52:12 -0400

    🔥 Removed separate categories array, computed directly from links array | 04/04

--- src/state/context/LinksContext.js
+++ src/state/context/LinksContext.js
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 export const LinksContext = React.createContext({ state: {}, dispatch: () => {} });
 
 export const LinksProvider = ({ children }) => {
-  const [{ links, categories }, dispatch] = linkReducer();
-  return <LinksContext.Provider value={[{ links, categories }, dispatch]}>{children}</LinksContext.Provider>;
+  const [{ links }, dispatch] = linkReducer();
+  return <LinksContext.Provider value={[{ links }, dispatch]}>{children}</LinksContext.Provider>;
 };
 
 LinksProvider.propTypes = {
