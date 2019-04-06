16a91b0c5490240f7aee6d8cef99ddf3e1a48bbb
Diego Vizhnay <diego@vzhny.com>
2019-04-06 11:34:44 -0400

    📝 Added toggle functions that animate respective components on mount/unmount | 04/06 📝 Added an Edit/Delete Links button to the side menu | 04/06

--- src/containers/SideMenu/SideMenu.js
+++ src/containers/SideMenu/SideMenu.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import clsx from 'clsx';
 import { AuthContext } from '@/state/context/AuthContext';
+import { EditLinksContext } from '@/state/context/EditLinksContext';
 import Backdrop from '@/components/Backdrop/Backdrop';
 import Modal from '@/components/Modal/Modal';
 import UserForm from '@/containers/UserForm/UserForm';
@@ -16,24 +17,55 @@ import {
 } from './SideMenu.module.scss';
 
 const SideMenu = () => {
-  const [sideMenuVisible, toggleSideMenu] = useState(false);
-  const [modalVisible, toggleModal] = useState(false);
+  const [sideMenuVisible, setSideMenu] = useState(false);
+  const [modalVisible, setModal] = useState(false);
+  const [animateSideMenu, toggleSideMenuAnimation] = useState(false);
+  const [animateModal, toggleModalAnimation] = useState(false);
   const [auth] = useContext(AuthContext);
+  const [, toggleEditLinks] = useContext(EditLinksContext);
 
-  const handleEscKey = e => {
-    const { keyCode } = e;
-    const escKey = 27;
-
-    if (keyCode === escKey) {
-      e.preventDefault();
-      toggleSideMenu(false);
-      window.removeEventListener('keyup', handleEscKey);
+  const toggleSideMenu = () => {
+    if (sideMenuVisible === true) {
+      toggleSideMenuAnimation(false);
+      setTimeout(() => setSideMenu(false), 500);
+    } else {
+      setSideMenu(true);
+      setTimeout(() => toggleSideMenuAnimation(true), 0);
     }
   };
 
-  const closeAfterClick = () => {
-    toggleModal(true);
-    toggleSideMenu(false);
+  const toggleModal = () => {
+    if (modalVisible === true) {
+      toggleModalAnimation(false);
+      setTimeout(() => setModal(false), 500);
+    } else {
+      setModal(true);
+      setTimeout(() => toggleModalAnimation(true), 0);
+    }
+  };
+
+  const handleEscKey = event => {
+    const { keyCode } = event;
+    const escKey = 27;
+
+    if (keyCode === escKey) {
+      event.preventDefault();
+
+      if (sideMenuVisible === true) {
+        toggleSideMenu();
+        window.removeEventListener('keyup', handleEscKey);
+      }
+    }
+  };
+
+  const closeAfterClick = (options = { toggleModal: true }) => {
+    if (options.toggleModal) {
+      toggleModal();
+    } else {
+      toggleEditLinks(true);
+    }
+
+    toggleSideMenu();
   };
 
   useEffect(() => {
@@ -47,31 +79,46 @@ const SideMenu = () => {
   return (
     <>
       <button
-        className={clsx(hamburgerMenu, sideMenuVisible && closeHamburgerMenu)}
-        onClick={() => toggleSideMenu(!sideMenuVisible)}
+        className={clsx(hamburgerMenu, animateSideMenu && closeHamburgerMenu)}
+        onClick={() => toggleSideMenu()}
         type="button"
       >
         <div className={line} />
         <div className={line} />
         <div className={line} />
       </button>
-      <Backdrop isVisible={sideMenuVisible} toggleVisibility={toggleSideMenu} />
-      <div className={clsx(sideMenu, sideMenuVisible && open)}>
-        <div className={sideMenuList}>
-          {auth ? (
-            <button className={sideMenuButton} onClick={() => closeAfterClick()} type="button">
-              Add a New Link
-            </button>
-          ) : (
-            <button className={sideMenuButton} onClick={() => closeAfterClick()} type="button">
-              Login/Register
-            </button>
-          )}
-        </div>
-      </div>
-      <Modal isVisible={modalVisible} toggleVisibility={toggleModal}>
-        {auth ? <AddLinkForm toggleVisibility={toggleModal} /> : <UserForm toggleVisibility={toggleModal} />}
-      </Modal>
+      {sideMenuVisible && (
+        <>
+          <Backdrop animate={animateSideMenu} toggleClose={toggleSideMenu} />
+          <div className={clsx(sideMenu, animateSideMenu && open)}>
+            <div className={sideMenuList}>
+              {auth ? (
+                <>
+                  <button className={sideMenuButton} onClick={() => closeAfterClick()} type="button">
+                    Add a New Link
+                  </button>
+                  <button
+                    className={sideMenuButton}
+                    onClick={() => closeAfterClick({ toggleModal: false })}
+                    type="button"
+                  >
+                    Edit/Delete Links
+                  </button>
+                </>
+              ) : (
+                <button className={sideMenuButton} onClick={() => closeAfterClick()} type="button">
+                  Login/Register
+                </button>
+              )}
+            </div>
+          </div>
+        </>
+      )}
+      {modalVisible && (
+        <Modal animate={animateModal} toggleClose={toggleModal}>
+          {auth ? <AddLinkForm toggleVisibility={setModal} /> : <UserForm toggleVisibility={setModal} />}
+        </Modal>
+      )}
     </>
   );
 };
