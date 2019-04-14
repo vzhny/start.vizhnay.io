2ed89d6c6cf3551df48807cbf6ffb2fcee6a0314
Diego Vizhnay <diego@vzhny.com>
2019-04-14 13:19:54 -0400

    📝 Added modal logic when clicking on delete/edit buttons | 04/14

--- src/containers/Link/Link.js
+++ src/containers/Link/Link.js
@@ -3,14 +3,28 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import to from 'await-to-js';
 import store from 'store';
+import useAnimation from '@/state/hooks/useAnimation';
 import { RefreshContext } from '@/state/context/RefreshContext';
 import { LinksContext } from '@/state/context/LinksContext';
 import { EditLinksContext } from '@/state/context/EditLinksContext';
 import EditIcon from '@/images/edit.svg';
 import DeleteIcon from '@/images/close.svg';
-import { linkContainer, link, buttonGroup, button, editButtonIcon, deleteButtonIcon } from './Link.module.scss';
+import Modal from '@/components/Modal/Modal';
+import Card, { CardHeader, CardBody } from '@/components/Card/Card';
+import {
+  linkContainer,
+  link,
+  buttonGroup,
+  button,
+  editButtonIcon,
+  deleteButtonIcon,
+  modalButtonGroup,
+  cancelButton,
+  submitButton,
+} from './Link.module.scss';
 
 const Link = ({ linkId, title, url, category, ...props }) => {
+  const [[modalVisible, animateModal], toggleModal] = useAnimation();
   const [canEditLinks] = useContext(EditLinksContext);
   const [refresh, toggleRefresh] = useContext(RefreshContext);
   const [, dispatch] = useContext(LinksContext);
@@ -42,10 +56,31 @@ const Link = ({ linkId, title, url, category, ...props }) => {
           <button className={button} onClick={() => {}} type="button">
             <img alt="Edit Icon" className={editButtonIcon} src={EditIcon} />
           </button>
-          <button className={button} onClick={() => deleteLink()} type="button">
+          <button className={button} onClick={() => toggleModal()} type="button">
             <img alt="Delete Icon" className={deleteButtonIcon} src={DeleteIcon} />
           </button>
         </div>
+      )}
+      {modalVisible && (
+        <Modal animate={animateModal} toggleClose={toggleModal}>
+          <Card>
+            <CardHeader>
+              <h3>Delete Link</h3>
+            </CardHeader>
+            <CardBody>
+              <p style={{ marginBottom: '1rem' }}>Are you sure you want to delete this link?</p>
+              <p style={{ fontWeight: '600' }}>{url}</p>
+              <div className={modalButtonGroup}>
+                <button className={cancelButton} onClick={() => toggleModal()} type="button">
+                  No
+                </button>
+                <button className={submitButton} onClick={() => deleteLink()} type="button">
+                  Yes
+                </button>
+              </div>
+            </CardBody>
+          </Card>
+        </Modal>
       )}
     </div>
   );
